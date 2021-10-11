37a78a529015351ab45f040619959a777e84568f
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-10-11 16:21:27 +0200

    Fix line pushed by mistake

--- core/log.class.inc.php
+++ core/log.class.inc.php
@@ -662,8 +662,6 @@ abstract class LogAPI
 		}
 		if (static::IsLogLevelEnabled($sLevel, $sChannel, static::ENUM_CONFIG_PARAM_DB)) {
 			self::WriteToDb($sMessage, $sChannel, $aContext);
-		} else {
-			static::$oLastEventIssue = null;
 		}
 	}
 
